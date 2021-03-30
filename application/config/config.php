b20b21817328b371a1bdd0a3ca23509f27f40f13
debyprast <debyprast18@gmail.com>
2021-03-30 22:50:16 +0700

    perbaikan load halaman

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost/Bismillah_Skripsi';
+$config['base_url'] = 'http://localhost/Bismillah_Skripsi';
 
 /*
 |--------------------------------------------------------------------------
