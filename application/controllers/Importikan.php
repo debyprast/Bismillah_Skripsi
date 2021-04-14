36c11568c7fc1ef00e7cb4263a08d380a8431616
debyprast <debyprast18@gmail.com>
2021-04-14 15:00:08 +0700

    import excel sukses

---
+++ application/controllers/Importikan.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+require_once APPPATH . 'third_party/Spout/Autoloader/autoload.php';
+
+use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
+
+class Importikan extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Barang_model');
+    }
+
+
+
+    public function index()
+    {
+		$data['title'] = 'Export Import';
+		$data['semuabarang'] = $this->Barang_model->getDataBarang();
+        
+		$this->load->view('template_admin/header');
+		$this->load->view('template_admin/sidebar');
+		$this->load->view('admin/data_ikan', $data);
+		$this->load->view('template_admin/footer');
+    
+    }
+
+	public function uploaddata()
+    {
+		
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = 'xlsx|xls';
+        $config['file_name'] = 'doc' . time();
+        $this->load->library('upload', $config);
+        if ($this->upload->do_upload('importexcel')) {
+            $file = $this->upload->data();
+            $reader = ReaderEntityFactory::createXLSXReader();
+
+            $reader->open('uploads/' . $file['file_name']);
+            foreach ($reader->getSheetIterator() as $sheet) {
+                $numRow = 1;
+                foreach ($sheet->getRowIterator() as $row) {
+                    if ($numRow > 1) {
+                        $databarang = array(
+                            'ikan'  => $row->getCellAtIndex(1),
+                            'penyakit'  => $row->getCellAtIndex(2),
+                            'date_created' => time(),
+                            'date_modified' => time(),
+                        );
+                        $this->Barang_model->import_data($databarang);
+                    }
+                    $numRow++;
+                }
+				$reader->close();
+                unlink('uploads/' . $file['file_name']);
+                $this->session->set_flashdata('pesan', 'import Data Berhasil');
+                redirect('importikan');
+            }
+        } else {
+            echo "Error :" . $this->upload->display_errors();
+        };
+    }
+}
--- application/views/admin/data_ikan.php
+++ application/views/admin/data_ikan.php
@@ -1,48 +1,43 @@
-<div class="card">
-              <div class="card-header">
-                <h3 class="card-title">DataTable with default features</h3>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
-								 <div class="row">
-								  <div class="col-sm-12 col-md-6">             
-								 </div> 
+<div class="container">
+		<div class="row mt-2">
+			<div class="col-12">
+				<div class="card">
+					<div class="card-body">
+						<?= form_open_multipart('Importikan/uploaddata')?>
+							<div class="form-row">
+								<div class="col-4">
+								<input type="file" class="form-control-file" id="importexcel" name="importexcel" accept=".xls, .xlsx">
 								</div>
-							 </div>
-								 <div class="col-sm-12 col-md-6">
-								  <div id="example1_filter" class="dataTables_filter">
-								   <label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example1"></label>
-								  </div>
-								 </div>
+								<div class="col">
+								<button type="submit" class="btn btn-primary">import</button>
 								</div>
-								 <div class="row">
-								 <div class="col-sm-12">
-								 <table id="example1" class="table table-bordered table-striped dataTable dtr-inline" role="grid" aria-describedby="example1_info">
-                  <thead>
-                  <tr role="row"><th class="sorting sorting_asc" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">Nomer</th>
-									<th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Id</th>
-									<th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Jenis Ikan</th>
-									</tr>
-                  </thead>
-                  <tbody>
-                  <tr class="odd">
-                    <td class="dtr-control sorting_1" tabindex="0">1</td>
-                    <td>19</td>
-                    <td>Arwana</td>
-                  </tr><tr class="even">
-                    <td class="dtr-control sorting_1" tabindex="0">2</td>
-                    <td>24</td>
-                    <td>Cupang</td>
-                     
-                  </tr><tr class="odd">
-                    <td class="dtr-control sorting_1" tabindex="0">3</td>
-                    <td>55</td>
-                    <td>Koi</td>
-                     
-										 </tbody>
-                  
-                </table></div></div><div class="row"><div class="col-sm-12 col-md-5"><div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div></div><div class="col-sm-12 col-md-7"><div class="dataTables_paginate paging_simple_numbers" id="example1_paginate"><ul class="pagination"><li class="paginate_button page-item previous disabled" id="example1_previous"><a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li><li class="paginate_button page-item active"><a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0" class="page-link">1</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0" class="page-link">2</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0" class="page-link">3</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0" class="page-link">4</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0" class="page-link">5</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0" class="page-link">6</a></li><li class="paginate_button page-item next" id="example1_next"><a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li></ul></div></div></div></div>
-              </div>
-              <!-- /.card-body -->
-            </div>
+								<div class="col">
+								<?= $this->session->flashdata('pesan');?>
+								</div>
+							</div>
+						<?= form_close();?>
+					</div>
+				</div>
+				<div class="card mt-2">
+                    <div class="card-body">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Jenis Ikan</th>
+                                    <th scope="col">Jenis Penyakit</th>
+                                    <th scope="col">Tanggal Input </th>
+                                    <th scope="col">Aksi</th>
+                                </tr>
+																<tbody>
+                                <?php $i = 1;
+                                foreach ($semuabarang as $barang) : ?>
+                                    <tr>
+                                        <td><?= $i++; ?></td>
+                                        <td><?= $barang['ikan']; ?></td>
+                                        <td><?= $barang['penyakit']; ?></td>
+                                        <td><?= date('d F Y', $barang['date_created']);  ?></td>
+                                        </tr> <?php endforeach; ?> </tbody>
+			</div>
+		</div>
+	</div>
